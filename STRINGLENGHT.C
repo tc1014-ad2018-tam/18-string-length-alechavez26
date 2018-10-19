b229f2ca874cdecf960658fa764d2f4445e96ada
Alejandra Chávez Cruz <42187730+alechavez26@users.noreply.github.com>
2018-10-19 03:05:17 -0500

    Add files via upload

---
+++ STRINGLENGHT.C
@@ -0,0 +1,30 @@
+﻿ *
+ *Alejandra Chavez Cruz
+ * A01411970@itesm.mx
+ * 18/Oct/18
+ */
+
+#include <stdio.h>
+
+int main() {
+    char string[200];
+    int x=1;
+    int counter=0;
+
+    // I ask for the string
+    printf("Give me a string: \n");
+    fgets(string, sizeof(string), stdin);
+
+
+    while (string[counter] != '\0') {
+        counter++;
+        if (string[counter] == ' '){
+            x++;
+        }
+    }
+
+    //  I show the user how many words has the string
+    printf("\nThe string has %i words",x);
+
+return 0;
+}
